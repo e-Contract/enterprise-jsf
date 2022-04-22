cd561122d63b513d520c030d9a1494779a5e35ca
Frank Cornelis <info@e-contract.be>
2022-04-22 12:26:43 +0200

    datatable

---
+++ helloworld/src/main/java/be/e_contract/jsf/helloworld/DataController.java
@@ -0,0 +1,45 @@
+package be.e_contract.jsf.helloworld;
+
+import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+@Named
+@ViewScoped
+public class DataController implements Serializable {
+
+    public static class Item {
+
+        private String name;
+        private String value;
+
+        public Item(String name, String value) {
+            this.name = name;
+            this.value = value;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+
+        public String getValue() {
+            return this.value;
+        }
+    }
+
+    private List<Item> items;
+
+    @PostConstruct
+    public void postConstruct() {
+        this.items = new LinkedList<>();
+        this.items.add(new Item("item 1", "value 1"));
+        this.items.add(new Item("item 2", "value 2"));
+    }
+
+    public List<Item> getItems() {
+        return this.items;
+    }
+}
