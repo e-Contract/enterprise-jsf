f4c234c410d4b19eaa5262aa2ae417c89b8bc322
Frank Cornelis <info@e-contract.be>
2022-05-08 09:06:30 +0200

    custom validator with parameter

---
+++ helloworld-ear/taglib/src/main/java/be/e_contract/jsf/taglib/ParameterValidator.java
@@ -0,0 +1,35 @@
+package be.e_contract.jsf.taglib;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+@FacesValidator("ejsf.parameterValidator")
+public class ParameterValidator implements Validator {
+
+    private String parameter;
+
+    public String getParameter() {
+        return this.parameter;
+    }
+
+    public void setParameter(String parameter) {
+        this.parameter = parameter;
+    }
+
+    @Override
+    public void validate(FacesContext facesContext, UIComponent component, Object value) throws ValidatorException {
+        if (null == value) {
+            return;
+        }
+        if (this.parameter.equals(value)) {
+            FacesMessage facesMessage
+                    = new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                            "foobar not allowed", null);
+            throw new ValidatorException(facesMessage);
+        }
+    }
+}
