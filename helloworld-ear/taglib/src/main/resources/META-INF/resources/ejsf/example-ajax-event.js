f0e19936bdd6a8eada9037bec24d0f5acaea56ac
Frank Cornelis <info@e-contract.be>
2022-05-14 09:41:45 +0200

    composite

--- helloworld-ear/taglib/src/main/resources/META-INF/resources/ejsf/example-ajax-event.js
+++ helloworld-ear/taglib/src/main/resources/META-INF/resources/ejsf/example-ajax-event.js
@@ -1,18 +1,14 @@
 function exampleAjaxEventOnClick(id, event, execute, render) {
     let options = {
-        'javax.faces.behavior.event': 'click'
+        "javax.faces.behavior.event": "click"
     };
     if (execute !== null) {
-        options['execute'] = execute;
+        options["execute"] = execute;
     }
     if (render !== null) {
-        options['render'] = render;
+        options["render"] = render;
     }
-    options.params = {
-    };
+    options.params = {};
     options.params[id + "_parameter"] = new Date().toString();
-    console.log("id: " + id);
-    console.log(event);
-    console.log(options);
     jsf.ajax.request(id, event, options);
 }
