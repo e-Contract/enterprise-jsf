86c554dfdf16c708aecd8ee10818f68ca7ef4814
Frank Cornelis <info@e-contract.be>
2022-05-29 11:02:50 +0200

    close dialog client behavior

---
+++ helloworld-primefaces/taglib/src/main/java/be/e_contract/jsf/taglib/CloseDialogClientBehavior.java
@@ -0,0 +1,25 @@
+package be.e_contract.jsf.taglib;
+
+import javax.faces.component.UIComponent;
+import javax.faces.component.behavior.ClientBehaviorBase;
+import javax.faces.component.behavior.ClientBehaviorContext;
+import javax.faces.component.behavior.FacesBehavior;
+import org.primefaces.component.dialog.Dialog;
+
+@FacesBehavior("ejsf.closeDialog")
+public class CloseDialogClientBehavior extends ClientBehaviorBase {
+
+    @Override
+    public String getScript(ClientBehaviorContext behaviorContext) {
+        UIComponent component = behaviorContext.getComponent();
+        while (!Dialog.class.isInstance(component)) {
+            component = component.getParent();
+            if (null == component) {
+                return null;
+            }
+        }
+        Dialog dialog = (Dialog) component;
+        String dialogWidgetVar = dialog.getWidgetVar();
+        return "PF('" + dialogWidgetVar + "').hide()";
+    }
+}
