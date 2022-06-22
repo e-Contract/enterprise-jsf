f90662c897142ab043f1360596507f4942b9006e
Frank Cornelis <info@e-contract.be>
2022-06-22 08:25:11 +0200

    spring boot example

---
+++ spring/src/main/java/be/e_contract/ejsf/spring/Application.java
@@ -0,0 +1,12 @@
+package be.e_contract.ejsf.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ spring/src/main/java/be/e_contract/ejsf/spring/HelloWorldBean.java
@@ -0,0 +1,14 @@
+package be.e_contract.ejsf.spring;
+
+import java.io.Serializable;
+import javax.faces.view.ViewScoped;
+import org.springframework.stereotype.Component;
+
+@Component
+@ViewScoped
+public class HelloWorldBean implements Serializable {
+
+    public String getValue() {
+        return "hello world";
+    }
+}
