ae8f58d2d0e914deb83028de98e74cce982bb4db
Frank Cornelis <info@e-contract.be>
2023-01-30 09:36:10 +0100

    11576: outputTextEditorHtml

---
+++ ejsf-demo/src/main/java/be/e_contract/demo/OutputTextEditorHtmlController.java
@@ -0,0 +1,17 @@
+/*
+ * Enterprise JSF project.
+ *
+ * Copyright 2023 e-Contract.be BV. All rights reserved.
+ * e-Contract.be BV proprietary/confidential. Use is subject to license terms.
+ */
+package be.e_contract.demo;
+
+import javax.inject.Named;
+
+@Named("outputTextEditorHtmlController")
+public class OutputTextEditorHtmlController {
+
+    public String getValue() {
+        return "<ul><li>test</li><li class=\"ql-indent-1\">second level</li></ul>";
+    }
+}
---
+++ ejsf-taglib/src/main/java/be/e_contract/ejsf/output/OutputTextEditorHtmlComponent.java
@@ -0,0 +1,48 @@
+/*
+ * Enterprise JSF project.
+ *
+ * Copyright 2021-2023 e-Contract.be BV. All rights reserved.
+ * e-Contract.be BV proprietary/confidential. Use is subject to license terms.
+ */
+package be.e_contract.ejsf.output;
+
+import java.io.IOException;
+import javax.faces.application.ResourceDependencies;
+import javax.faces.application.ResourceDependency;
+import javax.faces.component.FacesComponent;
+import javax.faces.component.UIOutput;
+import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
+
+@FacesComponent(OutputTextEditorHtmlComponent.COMPONENT_TYPE)
+@ResourceDependencies(value = {
+    @ResourceDependency(library = "primefaces", name = "texteditor/texteditor.css")
+})
+public class OutputTextEditorHtmlComponent extends UIOutput {
+
+    public static final String COMPONENT_TYPE = "ejsf.outputTextEditorHtml";
+
+    public static final String COMPONENT_FAMILY = "ejsf";
+
+    @Override
+    public String getFamily() {
+        return COMPONENT_FAMILY;
+    }
+
+    @Override
+    public void encodeEnd(FacesContext context) throws IOException {
+        ResponseWriter responseWriter = context.getResponseWriter();
+
+        String clientId = super.getClientId(context);
+        responseWriter.startElement("span", this);
+        responseWriter.writeAttribute("id", clientId, "id");
+        responseWriter.writeAttribute("class", "ql-editor", null);
+
+        Object value = getValue();
+        if (null != value) {
+            responseWriter.write(getValue().toString());
+        }
+
+        responseWriter.endElement("span");
+    }
+}
