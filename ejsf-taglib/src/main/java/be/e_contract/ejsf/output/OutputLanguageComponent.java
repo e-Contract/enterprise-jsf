7dc53a631980b5e0607df92dc9e7c24a9bf3fbbf
Frank Cornelis <info@e-contract.be>
2023-01-30 09:58:20 +0100

    11576: outputLanguage

---
+++ ejsf-taglib/src/main/java/be/e_contract/ejsf/output/OutputLanguageComponent.java
@@ -0,0 +1,50 @@
+/*
+ * Enterprise JSF project.
+ *
+ * Copyright 2021-2023 e-Contract.be BV. All rights reserved.
+ * e-Contract.be BV proprietary/confidential. Use is subject to license terms.
+ */
+package be.e_contract.ejsf.output;
+
+import java.io.IOException;
+import java.util.Locale;
+import javax.faces.component.FacesComponent;
+import javax.faces.component.UIInput;
+import javax.faces.component.UIOutput;
+import javax.faces.context.FacesContext;
+import javax.faces.context.ResponseWriter;
+
+@FacesComponent(OutputLanguageComponent.COMPONENT_TYPE)
+public class OutputLanguageComponent extends UIOutput {
+
+    public static final String COMPONENT_TYPE = "ejsf.outputLanguage";
+
+    public static final String COMPONENT_FAMILY = "ejsf";
+
+    public OutputLanguageComponent() {
+        setRendererType(null);
+    }
+
+    @Override
+    public String getFamily() {
+        return COMPONENT_FAMILY;
+    }
+
+    @Override
+    public void encodeEnd(FacesContext context) throws IOException {
+        ResponseWriter responseWriter = context.getResponseWriter();
+
+        String clientId = super.getClientId(context);
+        responseWriter.startElement("span", this);
+        responseWriter.writeAttribute("id", clientId, "id");
+
+        String language = (String) getValue();
+        if (!UIInput.isEmpty(language)) {
+            Locale locale = new Locale(language);
+            String displayLanguage = locale.getDisplayLanguage(locale);
+            responseWriter.write(displayLanguage);
+        }
+
+        responseWriter.endElement("span");
+    }
+}
