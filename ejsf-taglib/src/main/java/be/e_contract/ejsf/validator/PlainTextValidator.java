a2314ec889100153aafd8b278ef77fab8d4e7f03
Frank Cornelis <info@e-contract.be>
2023-01-30 17:32:35 +0100

    11576: plainTextValidator

---
+++ ejsf-taglib/src/main/java/be/e_contract/ejsf/validator/PlainTextValidator.java
@@ -0,0 +1,44 @@
+/*
+ * Enterprise JSF project.
+ *
+ * Copyright 2022-2023 e-Contract.be BV. All rights reserved.
+ * e-Contract.be BV proprietary/confidential. Use is subject to license terms.
+ */
+package be.e_contract.ejsf.validator;
+
+import java.util.ResourceBundle;
+import javax.faces.application.Application;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIInput;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+import org.owasp.html.HtmlPolicyBuilder;
+import org.owasp.html.PolicyFactory;
+
+@FacesValidator("ejsf.plainTextValidator")
+public class PlainTextValidator implements Validator {
+
+    @Override
+    public void validate(FacesContext facesContext, UIComponent component, Object value) throws ValidatorException {
+        if (null == value) {
+            return;
+        }
+        String strValue = (String) value;
+        if (UIInput.isEmpty(strValue)) {
+            return;
+        }
+        PolicyFactory policy = new HtmlPolicyBuilder().toFactory();
+        String safeHTML = policy.sanitize(strValue);
+        if (!safeHTML.equals(strValue)) {
+            Application application = facesContext.getApplication();
+            ResourceBundle resourceBundle = application.getResourceBundle(facesContext, "ejsfMessages");
+            String errorMessage = resourceBundle.getString("invalidCharacters");
+            FacesMessage facesMessage = new FacesMessage(errorMessage);
+            facesMessage.setSeverity(FacesMessage.SEVERITY_ERROR);
+            throw new ValidatorException(facesMessage);
+        }
+    }
+}
