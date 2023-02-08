6a0f1005ca9df897c41a650c972ede4f3dfb06d4
Frank Cornelis <info@e-contract.be>
2023-02-08 09:12:39 +0100

    11726: working Jakarta EE 10 demo

---
+++ ejsf-demo-ee10/src/main/java/be/e_contract/demo/ClockSyncController.java
@@ -0,0 +1,46 @@
+/*
+ * Enterprise JSF project.
+ *
+ * Copyright 2023 e-Contract.be BV. All rights reserved.
+ * e-Contract.be BV proprietary/confidential. Use is subject to license terms.
+ */
+package be.e_contract.demo;
+
+import be.e_contract.ejsf.clocksync.ClockSyncEvent;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
+import jakarta.inject.Named;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Named("clockSyncController")
+@RequestScoped
+public class ClockSyncController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ClockSyncController.class);
+
+    private Long bestRoundTripDelay;
+
+    private Long deltaT;
+
+    public void clockSynchronized(ClockSyncEvent event) {
+        this.bestRoundTripDelay = event.getBestRoundTripDelay();
+        this.deltaT = event.getDeltaT();
+        LOGGER.debug("best round trip delay: {} ms", this.bestRoundTripDelay);
+        LOGGER.debug("delta T: {} ms", this.deltaT);
+
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        String message = "Synchronized.";
+        FacesMessage facesMessage = new FacesMessage(FacesMessage.SEVERITY_INFO, message, null);
+        facesContext.addMessage(null, facesMessage);
+    }
+
+    public Long getBestRoundTripDelay() {
+        return this.bestRoundTripDelay;
+    }
+
+    public Long getDeltaT() {
+        return this.deltaT;
+    }
+}
