78984960b3f5b412c6e4e97f6f7be134b1ede451
Kim Scholte <kim@koding.ai>
2023-02-09 13:39:47 +0100

    11725 Spring: redirect from / to index.xhtml

---
+++ spring/src/main/java/be/e_contract/ejsf/spring/RedirectToIndexController.java
@@ -0,0 +1,14 @@
+package be.e_contract.ejsf.spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RedirectToIndexController {
+
+    @GetMapping("/")
+    public String index(){
+        return "redirect:index.xhtml";
+    }
+
+}
