9bcb860390462894e21361c334fa2efac6bac242
Frank Cornelis <info@e-contract.be>
2023-02-15 17:45:23 +0100

    11768: requiresRole validator

---
+++ ejsf-taglib/src/main/java/be/e_contract/ejsf/validator/RequiresRoleValidator.java
@@ -0,0 +1,104 @@
+/*
+ * Enterprise JSF project.
+ *
+ * Copyright 2023 e-Contract.be BV. All rights reserved.
+ * e-Contract.be BV proprietary/confidential. Use is subject to license terms.
+ */
+package be.e_contract.ejsf.validator;
+
+import java.util.ResourceBundle;
+import javax.faces.application.Application;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.StateHolder;
+import javax.faces.component.UIComponent;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+@FacesValidator(RequiresRoleValidator.VALIDATOR_ID)
+public class RequiresRoleValidator implements Validator, StateHolder {
+
+    public static final String VALIDATOR_ID = "ejsf.requiresRoleValidator";
+
+    private boolean _transient;
+
+    private String role;
+
+    private String message;
+
+    public String getRole() {
+        return this.role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public void validate(FacesContext facesContext, UIComponent component, Object value) throws ValidatorException {
+        if (null == value) {
+            return;
+        }
+        ExternalContext externalContext = facesContext.getExternalContext();
+        if (!externalContext.isUserInRole(this.role)) {
+            String errorMessage;
+            if (null != this.message) {
+                errorMessage = this.message;
+            } else {
+                Application application = facesContext.getApplication();
+                ResourceBundle resourceBundle = application.getResourceBundle(facesContext, "ejsfMessages");
+                errorMessage = resourceBundle.getString("requiresRole");
+            }
+            FacesMessage facesMessage = new FacesMessage(errorMessage);
+            facesMessage.setSeverity(FacesMessage.SEVERITY_ERROR);
+            throw new ValidatorException(facesMessage);
+        }
+    }
+
+    @Override
+    public Object saveState(FacesContext context) {
+        if (context == null) {
+            throw new NullPointerException();
+        }
+        return new Object[]{
+            this.role,
+            this.message
+        };
+    }
+
+    @Override
+    public void restoreState(FacesContext context, Object state) {
+        if (context == null) {
+            throw new NullPointerException();
+        }
+        if (state == null) {
+            return;
+        }
+        Object[] stateObjects = (Object[]) state;
+        if (stateObjects.length == 0) {
+            return;
+        }
+        this.role = (String) stateObjects[0];
+        this.message = (String) stateObjects[1];
+    }
+
+    @Override
+    public boolean isTransient() {
+        return this._transient;
+    }
+
+    @Override
+    public void setTransient(boolean newTransientValue) {
+        this._transient = newTransientValue;
+    }
+}
