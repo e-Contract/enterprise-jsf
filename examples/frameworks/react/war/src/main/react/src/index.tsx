479b51276d49fa4834bf6b08702f246f30c0c258
Frank Cornelis <info@e-contract.be>
2023-08-17 08:05:51 +0200

    doc: more fun with react

--- examples/frameworks/react/war/src/main/react/src/index.tsx
+++ examples/frameworks/react/war/src/main/react/src/index.tsx
@@ -1,14 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { PrimeReactProvider } from "primereact/api";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 import "primereact/resources/primereact.min.css";
-import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/themes/saga-blue/theme.css";
 import { Button } from "primereact/button";
 import { Dialog } from "primereact/dialog";
-import "primeflex/primeflex.css";
+import "primeflex/primeflex.min.css";
 import { InputText } from "primereact/inputtext";
 import { createRoot } from 'react-dom/client';
+import 'primeicons/primeicons.css';
+import { Messages } from 'primereact/messages';
 
 const App = () => {
     console.log("initialization...");
@@ -17,6 +19,8 @@ const App = () => {
     const [removeDialogVisible, setRemoveDialogVisible] = useState<boolean>(false);
     const [addDialogName, setAddDialogName] = useState<string>("");
     const [addDialogAmount, setAddDialogAmount] = useState<string>("");
+    const [removeItem, setRemoveItem] = useState<string>("");
+    const messages = useRef<Messages>(null);
 
     useEffect(() => {
         console.log("useEffect");
@@ -39,6 +43,10 @@ const App = () => {
             .then((response: Response) => {
                 console.log("response status: " + response.status);
                 if (response.status === 204) { // not in line with OpenAPI 200 status code
+                    messages.current!.show({
+                        severity: "info",
+                        summary: "Item " + addDialogName + " added."
+                    });
                     setAddDialogName("");
                     setAddDialogAmount("");
                     setAddDialogVisible(false);
@@ -47,28 +55,40 @@ const App = () => {
             });
     };
 
-    function ItemRemoveButton(rowData) {
-        function itemRemoveButtonClicked(rowData) {
-            console.log("remove: " + rowData.name);
-            fetch("http://localhost:8080/react/api/item/remove?name=" + rowData.name)
-                .then((response: Response) => {
-                    loadTableData();
+    function removeItemFunc() {
+        fetch("http://localhost:8080/react/api/item/remove?name=" + removeItem)
+            .then((response: Response) => {
+                setRemoveDialogVisible(false);
+                loadTableData();
+                messages.current!.show({
+                    severity: "info",
+                    summary: "Item " + removeItem + " removed."
                 });
-        };
+            });
+    }
+
+    function ItemRemoveButton(rowData) {
         return (
-            <Button label="Remove" onClick={() => itemRemoveButtonClicked(rowData)} />
+            <Button label="Remove" onClick={() => {
+                setRemoveItem(rowData.name);
+                setRemoveDialogVisible(true)
+            }} icon="pi pi-trash" />
         );
     }
 
     return (
         <PrimeReactProvider>
+            <Messages ref={messages} />
             <DataTable value={items}>
                 <Column field="name" header="Name" />
                 <Column field="amount" header="Amount" />
                 <Column header="Actions" body={ItemRemoveButton} />
             </DataTable>
-            <Button label="Add" onClick={() => setAddDialogVisible(true)} />
-            <Dialog header="Add Item" visible={addDialogVisible} onHide={() => setAddDialogVisible(false)}>
+            <Button label="Add"
+                onClick={() => setAddDialogVisible(true)}
+                className="mt-2" icon="pi pi-plus-circle" />
+            <Dialog header="Add Item" visible={addDialogVisible}
+                onHide={() => setAddDialogVisible(false)}>
                 <div className="field grid">
                     <label htmlFor="name" className="col-fixed">Name</label>
                     <div className="col">
@@ -85,17 +105,26 @@ const App = () => {
                             onChange={(e) => setAddDialogAmount(e.target.value)} />
                     </div>
                 </div>
-                <div>
-                    <Button label="Add" onClick={addItemOnClickListener} />
+                <div style={{ marginTop: "10px" }}>
+                    <Button label="Add" onClick={addItemOnClickListener}
+                        style={{ marginRight: "10px" }} icon="pi pi-plus-circle" />
                     <Button label="Dismiss" onClick={() => {
                         setAddDialogName("");
                         setAddDialogAmount("");
                         setAddDialogVisible(false)
-                    }} />
+                    }} icon="pi pi-times" />
                 </div>
             </Dialog>
-            <Dialog header="Remove Item" visible={removeDialogVisible} onHide={() => setRemoveDialogVisible(false)}>
-                TODO
+            <Dialog header="Remove Item"
+                visible={removeDialogVisible}
+                onHide={() => setRemoveDialogVisible(false)}>
+                Do you want to remove {removeItem}?
+                <div className="mt-2">
+                    <Button label="Remove" onClick={removeItemFunc}
+                        className="mr-2" icon="pi pi-trash" />
+                    <Button label="Dismiss" icon="pi pi-times"
+                        onClick={() => setRemoveDialogVisible(false)} />
+                </div>
             </Dialog>
         </PrimeReactProvider>
     );
