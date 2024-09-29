c68f52930533c68f3d03e0aea24c33ef47e1544a
Frank Cornelis <info@e-contract.be>
2024-09-29 09:14:02 +0200

    13355: addMessage

---
+++ ejsf-taglib/src/main/java/be/e_contract/ejsf/component/message/AddMessageActionListener.java
@@ -0,0 +1,142 @@
+/*
+ * Enterprise JSF project.
+ *
+ * Copyright 2024 e-Contract.be BV. All rights reserved.
+ * e-Contract.be BV proprietary/confidential. Use is subject to license terms.
+ */
+package be.e_contract.ejsf.component.message;
+
+import javax.el.ELContext;
+import javax.el.ValueExpression;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.StateHolder;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.event.AbortProcessingException;
+import javax.faces.event.ActionEvent;
+import javax.faces.event.ActionListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AddMessageActionListener implements ActionListener, StateHolder {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AddMessageActionListener.class);
+
+    private boolean _transient;
+
+    private String severity;
+
+    private ValueExpression summary;
+
+    private ValueExpression detail;
+
+    private String target;
+
+    public AddMessageActionListener() {
+        super();
+    }
+
+    public AddMessageActionListener(String severity, ValueExpression summary, ValueExpression detail, String target) {
+        this.severity = severity;
+        this.summary = summary;
+        this.detail = detail;
+        this.target = target;
+    }
+
+    @Override
+    public Object saveState(final FacesContext context) {
+        if (context == null) {
+            throw new NullPointerException();
+        }
+        return new Object[]{
+            this.severity,
+            this.summary,
+            this.detail,
+            this.target
+        };
+    }
+
+    @Override
+    public void restoreState(final FacesContext context, final Object state) {
+        if (context == null) {
+            throw new NullPointerException();
+        }
+        if (state == null) {
+            return;
+        }
+        final Object[] stateObjects = (Object[]) state;
+        if (stateObjects.length == 0) {
+            return;
+        }
+        this.severity = (String) stateObjects[0];
+        this.summary = (ValueExpression) stateObjects[1];
+        this.detail = (ValueExpression) stateObjects[2];
+        this.target = (String) stateObjects[3];
+    }
+
+    @Override
+    public boolean isTransient() {
+        return this._transient;
+    }
+
+    @Override
+    public void setTransient(final boolean newTransientValue) {
+        this._transient = newTransientValue;
+    }
+
+    private String getSummary(FacesContext facesContext) {
+        if (null == this.summary) {
+            return null;
+        }
+        ELContext elContext = facesContext.getELContext();
+        return (String) this.summary.getValue(elContext);
+    }
+
+    private String getDetail(FacesContext facesContext) {
+        if (null == this.detail) {
+            return null;
+        }
+        ELContext elContext = facesContext.getELContext();
+        return (String) this.detail.getValue(elContext);
+    }
+
+    private FacesMessage.Severity getSeverity() {
+        if (null == this.severity) {
+            return FacesMessage.SEVERITY_INFO;
+        }
+        switch (this.severity.toUpperCase()) {
+            case "INFO":
+                return FacesMessage.SEVERITY_INFO;
+            case "ERROR":
+                return FacesMessage.SEVERITY_ERROR;
+            case "FATAL":
+                return FacesMessage.SEVERITY_FATAL;
+            case "WARN":
+                return FacesMessage.SEVERITY_WARN;
+            default:
+                return FacesMessage.SEVERITY_INFO;
+        }
+    }
+
+    private String getTargetClientId(ActionEvent event) {
+        if (null == this.target) {
+            return null;
+        }
+        UIComponent component = event.getComponent();
+        UIComponent targetComponent = component.findComponent(this.target);
+        if (null == targetComponent) {
+            LOGGER.error("target not found: {}", this.target);
+            return null;
+        }
+        FacesContext facesContext = event.getFacesContext();
+        return targetComponent.getClientId(facesContext);
+    }
+
+    @Override
+    public void processAction(ActionEvent event) throws AbortProcessingException {
+        FacesContext facesContext = event.getFacesContext();
+        FacesMessage facesMessage = new FacesMessage(getSeverity(), getSummary(facesContext), getDetail(facesContext));
+        String clientId = getTargetClientId(event);
+        facesContext.addMessage(clientId, facesMessage);
+    }
+}
---
+++ ejsf-taglib/src/main/java/be/e_contract/ejsf/component/message/AddMessageTagHandler.java
@@ -0,0 +1,74 @@
+/*
+ * Enterprise JSF project.
+ *
+ * Copyright 2024 e-Contract.be BV. All rights reserved.
+ * e-Contract.be BV proprietary/confidential. Use is subject to license terms.
+ */
+package be.e_contract.ejsf.component.message;
+
+import java.io.IOException;
+import javax.el.ValueExpression;
+import javax.faces.component.ActionSource;
+import javax.faces.component.UIComponent;
+import javax.faces.view.facelets.ComponentHandler;
+import javax.faces.view.facelets.FaceletContext;
+import javax.faces.view.facelets.TagAttribute;
+import javax.faces.view.facelets.TagConfig;
+import javax.faces.view.facelets.TagException;
+import javax.faces.view.facelets.TagHandler;
+
+public class AddMessageTagHandler extends TagHandler {
+
+    public AddMessageTagHandler(TagConfig tagConfig) {
+        super(tagConfig);
+    }
+
+    @Override
+    public void apply(FaceletContext faceletContext, UIComponent parent) throws IOException {
+        if (parent == null) {
+            return;
+        }
+        if (!ComponentHandler.isNew(parent)) {
+            return;
+        }
+        if (!(parent instanceof ActionSource)) {
+            throw new TagException(this.tag, "Can only be attached to ActionSource components.");
+        }
+        ActionSource actionSource = (ActionSource) parent;
+
+        TagAttribute severityTagAttribute = getAttribute("severity");
+        String severity;
+        if (null != severityTagAttribute) {
+            severity = severityTagAttribute.getValue();
+        } else {
+            severity = null;
+        }
+
+        TagAttribute summaryTagAttribute = getAttribute("summary");
+        ValueExpression summary;
+        if (null != summaryTagAttribute) {
+            summary = summaryTagAttribute.getValueExpression(faceletContext, String.class);
+        } else {
+            summary = null;
+        }
+
+        TagAttribute detailTagAttribute = getAttribute("detail");
+        ValueExpression detail;
+        if (null != detailTagAttribute) {
+            detail = detailTagAttribute.getValueExpression(faceletContext, String.class);
+        } else {
+            detail = null;
+        }
+
+        TagAttribute targetTagAttribute = getAttribute("target");
+        String target;
+        if (null != targetTagAttribute) {
+            target = targetTagAttribute.getValue();
+        } else {
+            target = null;
+        }
+
+        AddMessageActionListener actionListener = new AddMessageActionListener(severity, summary, detail, target);
+        actionSource.addActionListener(actionListener);
+    }
+}
