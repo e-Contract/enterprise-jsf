120dac6ffc7addcd8fd8dea2b77dff2195c88693
Frank Cornelis <info@e-contract.be>
2025-08-15 10:48:30 +0200

    ECharts select/unselect all demo

--- ejsf-demo/src/main/webapp/resources/demo/echarts.js
+++ ejsf-demo/src/main/webapp/resources/demo/echarts.js
@@ -1,7 +1,7 @@
 /*
  * Enterprise JSF project.
  *
- * Copyright 2023 e-Contract.be BV. All rights reserved.
+ * Copyright 2023-2025 e-Contract.be BV. All rights reserved.
  * e-Contract.be BV proprietary/confidential. Use is subject to license terms.
  */
 
@@ -9,4 +9,26 @@ function resizeECharts(event) {
     let height = event.target.value;
     let echartsWidget = PF("demoECharts");
     echartsWidget.setHeight(height);
+    return false;
+}
+
+function unselectAllECharts() {
+    let echartsWidget = PF("demoECharts");
+    let echartsInstance = echartsWidget.getEChartsInstance();
+    echartsInstance.dispatchAction({
+        type: "legendAllSelect"
+    });
+    echartsInstance.dispatchAction({
+        type: "legendInverseSelect"
+    });
+    return false;
+}
+
+function selectAllECharts() {
+    let echartsWidget = PF("demoECharts");
+    let echartsInstance = echartsWidget.getEChartsInstance();
+    echartsInstance.dispatchAction({
+        type: "legendAllSelect"
+    });
+    return false;
 }
