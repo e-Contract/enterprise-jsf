d5310c38d00c579141237bc279741f378e67f806
Frank Cornelis <info@e-contract.be>
2025-11-01 10:42:12 +0100

    dependency updates

---
+++ src/main/scripts/macos-env-java21.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk-21.jdk/Contents/Home"
+
+if [ ! -d "$JAVA_HOME" ]
+then
+    echo "$JAVA_HOME not found"
+    exit 1
+fi
+
+export PATH="$JAVA_HOME/bin:$PATH"
+java -version
+javac -version
