018ac245bc2605ab16d127d549cb9061843e3e39
Frank Cornelis <info@e-contract.be>
2025-11-21 10:19:02 +0100

    14041: also highlight plaintext, python, and bash

--- ejsf-taglib-highlight/src/main/node/index.js
+++ ejsf-taglib-highlight/src/main/node/index.js
@@ -1,7 +1,7 @@
 /*
  * Enterprise JSF project.
  *
- * Copyright 2023-2024 e-Contract.be BV. All rights reserved.
+ * Copyright 2023-2025 e-Contract.be BV. All rights reserved.
  * e-Contract.be BV proprietary/confidential. Use is subject to license terms.
  */
 
@@ -11,4 +11,7 @@ hljs.registerLanguage("xml", require("highlight.js/lib/languages/xml"));
 hljs.registerLanguage("json", require("highlight.js/lib/languages/json"));
 hljs.registerLanguage("java", require("highlight.js/lib/languages/java"));
 hljs.registerLanguage("markdown", require("highlight.js/lib/languages/markdown"));
+hljs.registerLanguage("plaintext", require("highlight.js/lib/languages/plaintext"));
+hljs.registerLanguage("python", require("highlight.js/lib/languages/python"));
+hljs.registerLanguage("bash", require("highlight.js/lib/languages/bash"));
 module.exports = hljs;
