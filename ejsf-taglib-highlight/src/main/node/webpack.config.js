d2b7907f6a957196e722b18d7d6c0979ea1e2c23
Frank Cornelis <info@e-contract.be>
2026-01-20 20:56:52 +0100

    14075: owasp-java-html-sanitizer update

--- ejsf-taglib-highlight/src/main/node/webpack.config.js
+++ ejsf-taglib-highlight/src/main/node/webpack.config.js
@@ -1,3 +1,10 @@
+/*
+ * Enterprise JSF project.
+ *
+ * Copyright 2025-2026 e-Contract.be BV. All rights reserved.
+ * e-Contract.be BV proprietary/confidential. Use is subject to license terms.
+ */
+
 export default {
     entry: "./index.js",
     output: {
